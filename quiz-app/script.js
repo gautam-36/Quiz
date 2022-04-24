2dda96540065e5a5a94b2562960927eff0fd0815
Gautam Gupta <87703240+codewithgautamgupta@users.noreply.github.com>
2022-04-24 12:56:39 +0530

    js changes

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,48 +1,112 @@
 const quizData = [
     {
-        question: 'How old is Gautam Gupta',
-        a: '10',
-        b: '21',
-        c: '18',
-        d: '25',
-        correct: 'b'
-    },
-
-    {
-        question: 'what is the most used programming language in 2022',
-        a: 'java',
-        b: 'c#',
-        c: 'JavaScript',
-        d: 'Pyhton',
+        question: 'Anuarg yadav loves :',
+        a: 'Akshra',
+        b: 'Amrita',
+        c: 'Archana Yadav',
+        d: 'Sonam Malik',
         correct: 'c'
     },
 
     {
-        question: 'who is the PrimeMinister of India',
-        a: 'Narendra Modi',
-        b: 'Rahul Gandhi',
-        c: 'Amit Sah',
-        d: 'Dr. ManMohan Singh',
-        correct: 'a'
+        question: 'Aman Verma  loves',
+        a: 'Archana Yadav',
+        b: 'Amrita',
+        c: 'Akshra',
+        d: 'Sonam Malik',
+        correct: 'b'
     },
 
     {
-        question: 'what does HTML stands for ? ',
-        a: 'Aplication inteface programming',
-        b: 'Jason Object Notation',
-        c: 'CasCading Style Sheet',
-        d: 'HyperText Markup Language',
+        question: 'Abhishek pandey  loves',
+        a: 'Amrita',
+        b: 'Archana Yadav',
+        c: 'Khyati Soni',
+        d: 'Jyoti',
         correct: 'd'
     },
 
+
     {
-        question: 'JavaScript Launch in year ? ',
-        a: '1996',
-        b: '1995',
-        c: '1994',
-        d: 'none of the above',
+        question: 'Ajeet Giri loves',
+        a: 'Shreya Aggarwal',
+        b: 'Nishu khan',
+        c: 'Meenu',
+        d: 'Jyoti',
+        correct: 'c'
+    },
+
+    {
+        question: 'Akshay loves',
+        a: 'Shreya Aggarwal',
+        b: 'Nishu khan',
+        c: 'Meenu',
+        d: 'Akshra Kaushik',
+        correct: 'd'
+    },
+
+
+    {
+        question: 'Ankit yadav loves',
+        a: 'Shreya Aggarwal',
+        b: 'Deeya',
+        c: 'Meenu',
+        d: 'Akshra Kaushik',
         correct: 'b'
     },
+
+
+    {
+        question: 'Sandeep singh loves',
+        a: 'Shreya Aggarwal',
+        b: 'Deeya',
+        c: 'Sakshi yadav',
+        d: 'Akshra Kaushik',
+        correct: 'c'
+    },
+
+
+    {
+        question: 'Ashwani Deo loves',
+        a: 'Sonam',
+        b: 'Deeya',
+        c: 'Sakshi',
+        d: 'Bus ladki honi chahiye isko to sbse pyar ho jata hai',
+        correct: 'd'
+    },
+
+
+    {
+        question: 'Khuswant loves',
+        a: 'Sonam',
+        b: 'Deeya',
+        c: 'mechanical wali se',
+        d: 'Akshra Kaushik',
+        correct: 'c'
+    },
+
+
+    {
+        question: 'Prashant loves',
+        a: 'Sonam',
+        b: 'Deeya',
+        c: 'Maths Teacher (Anju) se ',
+        d: 'Akshra Kaushik',
+        correct: 'c'
+    },
+
+
+    {
+        question: 'Swastik loves',
+        a: 'Sonam',
+        b: 'Niharika',
+        c: 'Nishu khan ',
+        d: 'Ye Randwa hi Marega',
+        correct: 'd'
+    },
+
+
+
 ]
 
 const questionEl = document.getElementById('question');
@@ -104,9 +168,15 @@ submitBtn.addEventListener('click', () => {
             loadQuiz();
         }
         else {
-                quiz.innerHTML =
+            quiz.innerHTML =
+
                 `<h2> you answered correctly at ${score}/ ${quizData.length} </h2> 
-                <button onclick="location.reload()" > Reload</button>`;
+
+            <button onclick="location.reload()" > Reload</button>`;
+
+            if (score == quizData.length) {
+                window.alert('abe lodu khud ke liye bhi Set kr le sabka to pta hi h tujhe apna bhi kuchh kr le ');
+            }
         }
     }
 });
